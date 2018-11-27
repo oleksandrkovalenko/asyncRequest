f709940d21a9048ed5825deafa613fc9cd884d73
Oleksandr Kovalenko <oleksandr.kovalenko@auto1.com>
2018-11-27 12:48:40 +0100

    Refactor

--- src/main/java/de/welt/Main.java
+++ src/main/java/de/welt/Main.java
@@ -14,15 +14,30 @@ public class Main {
 
     private static final String DOMAIN = "https://jsonplaceholder.typicode.com";
 
-    public static void main(String[] args) throws IOException {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
         String userId = "1";
 
-        ObjectMapper mapper = new ObjectMapper();
+        Main main = new Main();
+        main.requestForUser(userId);
+
+    }
+
+    private void requestForUser(String userId) {
+        try {
+            requestForUserWithThrow(userId);
+        } catch (IOException e) {
+            //TODO: Add logging here
+        }
+    }
+
+    private void requestForUserWithThrow(String userId) throws IOException {
         User user = mapper.readValue(new URL(DOMAIN + "/users/" + userId), User.class);
         List<Post> posts = mapper.readValue(new URL(DOMAIN + "/posts?userId=" + userId), new TypeReference<List<Post>>(){});
 
         MergedResponse mergedResponse = new MergedResponse(user, posts);
-        String response = mapper.writeValueAsString(mergedResponse);
+        String response = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(mergedResponse);
 
         System.out.println(response);
     }
