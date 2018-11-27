34bd20ad351f682de7e4b3bc3896613483a5fe20
Oleksandr Kovalenko <oleksandr.kovalenko@auto1.com>
2018-11-27 12:59:10 +0100

    Make request async

--- src/main/java/de/welt/Main.java
+++ src/main/java/de/welt/Main.java
@@ -9,12 +9,16 @@ import de.welt.domain.User;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
 
     private static final String DOMAIN = "https://jsonplaceholder.typicode.com";
+    private static final Long TIMEOUT = 1000L;
 
     private ObjectMapper mapper = new ObjectMapper();
+
+    private ExecutorService executor = Executors.newFixedThreadPool(2);
 
     public static void main(String[] args) {
         String userId = "1";
@@ -27,18 +31,31 @@ public class Main {
     private void requestForUser(String userId) {
         try {
             requestForUserWithThrow(userId);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            //TODO: Add logging here
+        } catch (TimeoutException e) {
             //TODO: Add logging here
         }
     }
 
-    private void requestForUserWithThrow(String userId) throws IOException {
-        User user = mapper.readValue(new URL(DOMAIN + "/users/" + userId), User.class);
-        List<Post> posts = mapper.readValue(new URL(DOMAIN + "/posts?userId=" + userId), new TypeReference<List<Post>>(){});
+    private void requestForUserWithThrow(String userId) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        Future<User> userFuture = executor.submit(createUserCallable(userId));
+        Future<List<Post>> postFuture = executor.submit(createPostsCallable(userId));
+
+        User user = userFuture.get(TIMEOUT, TimeUnit.MILLISECONDS);
+        List<Post> posts = postFuture.get(TIMEOUT, TimeUnit.MILLISECONDS);
 
         MergedResponse mergedResponse = new MergedResponse(user, posts);
         String response = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(mergedResponse);
 
         System.out.println(response);
+    }
+
+    private Callable<User> createUserCallable(String userId) {
+        return () -> mapper.readValue(new URL(DOMAIN + "/users/" + userId), User.class);
+    }
+
+    private Callable<List<Post>> createPostsCallable(String userId) {
+        return () -> mapper.readValue(new URL(DOMAIN + "/posts?userId=" + userId), new TypeReference<List<Post>>(){});
     }
 }
